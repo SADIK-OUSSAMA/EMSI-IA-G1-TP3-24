0c1ab3885a7bbe31f883225a831a3776a2b847c1
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-11-09 22:48:10 +0100

    Création de projet

---
+++ src/main/java/ma/emsi/sadik/tp3sadik/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.sadik.tp3sadik;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}

25c5f3fe5e49cdc460d087b9a547108464edc71c
SADIK Oussama <Oussama.sadik@emsi-edu.ma>
2025-11-10 11:48:56 +0100

    Modifier le nom pour RestApplication

---
+++ src/main/java/ma/emsi/sadik/tp3sadik/RestApplication.java
@@ -0,0 +1,8 @@
+package ma.emsi.sadik.tp3sadik;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
